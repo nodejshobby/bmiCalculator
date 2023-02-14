00640628aaaa27595ac86f2f0fb79482a0a3d2fa
nodejshobby <nodejshobby@gmail.com>
2023-02-14 08:07:34 +0100

    Built a react BMI Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [bmi, setBmi] = useState("");
+  const calculateBmi = (e) => {
+    e.preventDefault();
+    let bmiValue;
+    bmiValue = weight / (height / 100) ** 2;
+    setBmi(bmiValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <div className="form-box">
+        <form onSubmit={calculateBmi}>
+          <h2>BMI CALCULATOR</h2>
+
+          <div className="bmi">
+            Your Body Mass Index is:{" "}
+            <b>
+              {bmi} Kg/m
+              <sup>2</sup>
+            </b>
+          </div>
+          <input
+            className="form-input"
+            type="number"
+            onChange={(e) => {
+              setWeight(e.target.value);
+            }}
+            placeholder="Weight in Kilogram"
+          ></input>
+          <input
+            className="form-input"
+            type="number"
+            placeholder="Height in Centimeter"
+            onChange={(e) => {
+              setHeight(e.target.value);
+            }}
+          ></input>
+          <button type="submit" className="form-button-primary">
+            Calculate
+          </button>
+          <button type="reset" className="form-button-outline mt-sm mb-sm">
+            Reset
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
